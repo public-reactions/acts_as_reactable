d367eb170964a37552e25b9dbb46df12846236cb
Nami.W <1160688+namiwang@users.noreply.github.com>
2022-04-09 15:54:35 +0800

    fix: enforce standard

--- lib/acts_as_reactable/reactable.rb
+++ lib/acts_as_reactable/reactable.rb
@@ -11,15 +11,15 @@ module ActsAsReactable
         has_many :reactions, class_name: "ActsAsReactable::Reaction", as: :reactable, dependent: :delete_all
 
         define_method :destroy_reaction_from do |reactor|
-          raise 'InvalidReactor' if !reactor
+          raise "InvalidReactor" if !reactor
 
           puts reaction = reactions.find_by(reactor: reactor)
-          return unless reaction = reactions.find_by(reactor: reactor)
+          return unless (reaction = reactions.find_by(reactor: reactor))
           reaction.destroy
         end
 
         define_method :update_reaction_from do |reactor, emoji = nil|
-          raise 'InvalidReactor' if !reactor
+          raise "InvalidReactor" if !reactor
 
           if !emoji
             return destroy_reaction_from(reactor)
