2b6aef3379da7d3aa9a969440720b04d005a78af
Nami.W <1160688+namiwang@users.noreply.github.com>
2022-04-19 00:17:33 +0800

    fix: remove needless validators

--- lib/acts_as_reactable/reaction.rb
+++ lib/acts_as_reactable/reaction.rb
@@ -5,8 +5,6 @@ module ActsAsReactable
     belongs_to :reactable, polymorphic: true
     belongs_to :reactor, polymorphic: true
 
-    validates_presence_of :reactable_id
-    validates_presence_of :reactor_id
     validates_format_of :emoji, with: Unicode::Emoji::REGEX
   end
 end
